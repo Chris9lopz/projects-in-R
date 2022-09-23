44fec01b140bf8c7acd9c0de9db4749919b763e6
Christian Lopez <christiand.lopezp@ecci.edu.co>
2022-09-22 21:21:59 -0500

    Se realiza recorrido de ciclo para cada horario y se condiciona y se selecciona el plan con mayor ganancia

--- taller2_estocaicos.R
+++ taller2_estocaicos.R
@@ -18,7 +18,7 @@ horarios <- c(unique(usuarios))
 # Ordenar el arreglo
 horarios <- sort(horarios,decreasing = FALSE)
 
-#Crear arreglo vacio
+#Crear arreglo vacio para lueg llenar los valores unicos
 contar_usuarios <- c()
 
 # Se recorre los valores únicos
@@ -34,5 +34,53 @@ matrix <- cbind(horarios,contar_usuarios)
 
 # Creamos tabla de los datos
 dataf <- data.frame(matrix)
+
+# Creación de variables acumuladoras
+suma_a <- 0
+suma_b <- 0
+suma_c <- 0
+contar <- 1 # Variable de recorrido a las posiciones de campo contar usuario
+
+# Recorrido de cada horario
+for(horario in dataf$horarios){
+  
+  # Condicionar los horarios de franja 7 a 10 hrs
+  if(horario == "7 hrs" || horario == "8 hrs" || horario == "9 hrs" || horario == "10 hrs"){
+    suma_a <- suma_a + as.numeric(dataf$contar_usuarios[contar])
+    contar <- contar + 1
+  }
+ 
+   # Condicionar los horarios de franja 11 a 15 hrs
+  if(horario == "11 hrs" || horario == "12 hrs" || horario == "13 hrs" || horario == "14 hrs" || horario == "15 hrs"){
+    suma_b <- suma_b + as.numeric(dataf$contar_usuarios[contar])
+    contar <- contar + 1
+  }
+  
+  # COndicionar los horarios de franja 16 a 18 hrs
+  if(horario == "16 hrs" || horario == "17 hrs" || horario == "18 hrs"){
+    suma_c <- suma_c + as.numeric(dataf$contar_usuarios[contar])
+    contar <- contar + 1
+  }
+}
+# Creacion de variables con calculo de tarifa para plan A
+plan_a_hor1 <- suma_a * 20000
+plan_a_hor2 <- suma_b * 30000
+plan_a_hor3 <- suma_c * 10000
+
+# Creacion de variables con calculo de tarifa para plan B
+plan_b_hor1 <- suma_a * 20000
+plan_b_hor2 <- suma_b * 15000
+plan_b_hor3 <- suma_c * 20000
+
+# Creación de variables suma de totales por plan A y plan B
+total_plan_a <- sum(plan_a_hor1,plan_a_hor2,plan_a_hor3)
+total_plan_b <- sum(plan_b_hor1, plan_b_hor2, plan_b_hor3)
+
+# Condiciona cual es el mejor plan e imprime en pantalla
+if(total_plan_a > total_plan_b){
+  print(paste("El plan que ganaría más dinero sería el plan A con un total de", total_plan_a))
+} else {
+  print(paste("El plan que ganaría más dinero sería el plan B con un total de", total_plan_b))
+}
 
 
